c77f00b0c6360fb875b3f955b1c68d5661179d16
ludovicMAK <ludovic93mak@gmail.com>
2023-08-14 10:39:33 +0200

    installation de express-handlebars

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express')
+const path = require("path")
+const { engine } = require("express-handlebars")
+app.engine('handlebars', engine());
+app.set('view engine','handlebars');
+//app.set('views',path.join(__dirname,"views"));
+
+//app.use(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname,"public")));
 const app = express()
 
 app.get('/', function (req, res) {
