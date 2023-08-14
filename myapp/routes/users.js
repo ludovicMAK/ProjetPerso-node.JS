84dc8c3c5c4bb3d015498317132e1b6dfbece860
ludovicMAK <ludovic93mak@gmail.com>
2023-08-14 16:30:46 +0200

    création du formulaire, et connection mongosse

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/inscription', function(req, res, next) {
+  res.render('formulaire');
 });
+
+router.post('/submit',function(req,res,next){
+  const { nom, email, message } = req.body;
+  console.log(req.body);
+  res.send('Données reçues avec succès !');
+})
 
 module.exports = router;
