441e0df64ed477f9a19f8b1863801a49e576c81a
ludovicMAK <ludovic93mak@gmail.com>
2023-08-14 14:40:09 +0200

    page js et css lier et inscription en cours

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ app.engine('handlebars', engine());
 app.set('view engine','handlebars');
 //app.set('views',path.join(__dirname,"views"));
 
-//app.use(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname,"views")));
 app.use(express.static(path.join(__dirname,"public")));
 
-
+app.get('/inscription',function(req,res){
+  console.log(res.render('inscription'));
+})
+app.post('/inscription',function(req,res){
+ console.log(req);
+ res.send("je suis la");
+})
 app.get('/login', function (req, res) {
   console.log(res.render('login'));
   
