63f04e855a068580ad2406c4deca21742cfd5f1a
ludovicMAK <ludovic93mak@gmail.com>
2023-08-14 10:58:48 +0200

    mise en place des views

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express')
 const path = require("path")
 const { engine } = require("express-handlebars")
+const express = require('express')
+const app = express()
 app.engine('handlebars', engine());
 app.set('view engine','handlebars');
 //app.set('views',path.join(__dirname,"views"));
 
 //app.use(express.static(path.join(__dirname,"views")));
 app.use(express.static(path.join(__dirname,"public")));
-const app = express()
+
 
 app.get('/', function (req, res) {
-  res.render('home');
+  console.log(res.render('home'));
+  
 })
 
 app.listen(3000)
