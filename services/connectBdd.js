61dd26d294ea5090d19bd84b1ef7a15ff03ef305
ludovicMAK <ludovic93mak@gmail.com>
2023-08-15 19:01:01 +0200

    installation du module mongoose et de dotenv, connection à la base de donnée réussi

--- services/connectBdd.js
+++ services/connectBdd.js
@@ -1,1 +1,9 @@
-const mongoose = require('mon')
+const mongoose = require('mongoose');
+require('dotenv').config();
+console.log(process.env.MONGO_URL);
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  
+}
