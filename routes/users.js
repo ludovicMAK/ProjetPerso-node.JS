c573286d52aa02620b6cf499c43bfd3f911dd7e7
ludovicMAK <ludovic93mak@gmail.com>
2023-08-15 18:19:45 +0200

    installation de mongoose et création de la route inscription + formulaire

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/inscription',function(req,res,next){
+  res.render('inscription');
+})
+
 module.exports = router;
