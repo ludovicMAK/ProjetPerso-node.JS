08d3fabaf35670c1dc961e737bd9ea61852bce08
ludovicMAK <ludovic93mak@gmail.com>
2023-08-16 21:19:06 +0200

    création du schéma user

---
+++ models/usersModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const {schema} = mongoose;
+
+const userSchema = new mongoose.Schema({
+    nom:{
+        type:String,
+        required:true
+    },
+    prenom:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const User = mongoose.model('user',userSchema);
+
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/usersModel');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,14 @@ router.get('/', function(req, res, next) {
 router.get('/inscription',function(req,res,next){
   res.render('inscription');
 })
+
+// router.post('/inscription', async function(req,res,next){
+//   console.log(req.body);
+//   const user = new User({nom:'mak',
+//                               prenom:'ludovic',
+//                               email:'ludovic93mak@gmail.com',
+//                               password:'root1234'});
+//   await user.save();
+// })
 
 module.exports = router;
